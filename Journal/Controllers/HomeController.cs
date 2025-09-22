9a0801b17518745e1a4b8184b134ee2bb53fe082
efanin <efanin.igor@gmail.com>
2025-09-22 21:30:39 +0300

    add db

--- Journal/Controllers/HomeController.cs
+++ Journal/Controllers/HomeController.cs
@@ -4,13 +4,17 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Security.Claims;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Text;
+using Microsoft.Data.SqlClient;
+
 
 namespace Journal.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        private readonly SqlConnection connection = new("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\nadyb\\source\\repos\\Journal\\DataBase\\Database.mdf;Integrated Security=True");
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -29,6 +33,23 @@ namespace Journal.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(UserModel user)
         {
+            await Console.Out.WriteLineAsync(user.Email);
+            await Console.Out.WriteLineAsync(user.Password);
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+              password: user.Password,
+              salt: Encoding.Unicode.GetBytes("CGYzxeN4plZekNC14Umm1Q22"),
+              prf: KeyDerivationPrf.HMACSHA256,
+              iterationCount: 100000,
+              numBytesRequested: 256 / 8));
+            await Console.Out.WriteLineAsync(hashed);
+
+            SqlCommand command = new(
+                "select * from Students where Email=@Email and Password=@Password",
+                connection
+                );
+            command.Parameters.AddWithValue("Email",user.Email);
+            command.Parameters.AddWithValue("Password", user.Password);
+
             var claims = new List<Claim>
                 {
                     new Claim(ClaimTypes.Name, user.Email),
